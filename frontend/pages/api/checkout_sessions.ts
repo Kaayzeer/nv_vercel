41a3a55fdfdf8fa6f18d5d3e89d5404f9aa302e4
nikolas.rosinelli@weknowit.se <mautiagiro@gmail.com>
2022-04-22 19:02:25 +0200

    front end stripe functionality test

---
+++ frontend/pages/api/checkout_sessions.ts
@@ -0,0 +1,28 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req: any, res: any) {
+  if (req.method === "POST") {
+    try {
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: "price_1KrIrqBvHU5UmnuthJ9SYMGo",
+            quantity: 1,
+          },
+        ],
+        payment_method_types: ["card"],
+        mode: "payment",
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      });
+      res.redirect(303, session.url);
+    } catch (err: any) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
