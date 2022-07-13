8df4077b67d6ab2dae526ea53e77c6d53020b7f7
Hadi Ansari <hadan326@student.liu.se>
2022-07-13 15:18:15 +0200

    Decreased distance between infocard (gap-5) -> (gap-4)

--- frontend/components/Card/InfoCard.tsx
+++ frontend/components/Card/InfoCard.tsx
@@ -28,7 +28,7 @@ type Props = {};
 export default function InfoCard({}: Props) {
   return (
     <div className="w-full 2xl:w-2/3 2xl:mx-auto customContainer">
-      <div className="lg:grid-col3  place-items-center -mt-40 px-4 md:px-0 gap-5">
+      <div className="lg:grid-col3 place-items-center -mt-40 px-4 md:px-0 gap-4">
         {cardsInfo.map((cardInfo, idx) => (
           <div
             key={idx}
